2999bf88df7d8bddc39be35f2c46e168237840a3
Luis C <luis@catacolabs.com>
2022-12-08 14:33:55 -0500

    First commit

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from transformers import pipeline
 import torch
+import whisper
+import os
+import base64
+from io import BytesIO
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    #medium, large-v1, large-v2
+    model_name = "medium"
+    model = whisper.load_model(model_name)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -15,12 +18,17 @@ def inference(model_inputs:dict) -> dict:
     global model
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    mp3BytesString = model_inputs.get('mp3BytesString', None)
+    if mp3BytesString == None:
+        return {'message': "No input provided"}
+    
+    mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
+    with open('input.mp3','wb') as file:
+        file.write(mp3Bytes.getbuffer())
     
     # Run the model
-    result = model(prompt)
-
+    result = model.transcribe("input.mp3")
+    output = {"text":result["text"]}
+    os.remove("input.mp3")
     # Return the results as a dictionary
-    return result
+    return output
